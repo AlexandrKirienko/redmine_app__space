b26171523612593f836a975b3314e88370f33240
Aleksandr <15874857+AlexandrKirienko@users.noreply.github.com>
2021-06-13 22:54:51 +0300

    5.2 suka

--- db/migrate/001_create_applications.rb
+++ db/migrate/001_create_applications.rb
@@ -1,4 +1,4 @@
-class CreateApplications < ActiveRecord::Migration
+class CreateApplications < ActiveRecord::Migration[5.2]
 
   def self.up
     add_column :users, :apps, :string, :default => '', :null => false
